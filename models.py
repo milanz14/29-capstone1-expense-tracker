3f58f402e76a9e1b93a8ba09cd66b14e46caf71d
milanzagorac <zagoracm@gmail.com>
2021-06-17 20:36:50 -0400

    added UUID for transaction ID

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from datetime import date
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -40,13 +42,13 @@ class UserTransaction(db.Model):
     """ all of a User's transactions """
     __tablename__ = 'users_transactions'
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
-    transaction_id = db.Column(db.Integer, db.ForeignKey('transactions.id'), primary_key=True)
+    transaction_id = db.Column(UUID(as_uuid=True), db.ForeignKey('transactions.id'), primary_key=True)
 
 class Transaction(db.Model):
     """ Transaction model """
     """ One user can have many transactions """
     __tablename__ = 'transactions'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     location = db.Column(db.Text, nullable=False)
     amount = db.Column(db.Integer, nullable=False)
     category = db.Column(db.Text, nullable=False)
